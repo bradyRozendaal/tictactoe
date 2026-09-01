debd3b62fa8318e904506cfa3fb06fb0de56266a
U-BSD\470103 <470103@SHS-FYBF6X3.beaverton.k12.or.us>
2026-09-01 10:21:47 -0700

    print function

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -8,7 +8,26 @@ using namespace std;
   Date: 
  */
 
+void printBoard(char board[][])
+{
+  cout << " 1 2 3";
+  char lines[3] = ['a', 'b', 'c']
+  for (int i = 0; i < 3; i++)
+    {
+      for (int j = 0; j < 3; j++)
+	{
+	  if (j == 0)
+	    {
+	      cout << "\n" << lines[i] << " ";
+	    }
+	  cout << board[j][i] << " " <<;
+	}
+    }
+}
+
 int main()
 {
+  char board[3][3];
+  
   return 0;
 }
